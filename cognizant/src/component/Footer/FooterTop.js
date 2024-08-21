9a1ac8f2eba7fa55b325b5aea51d836843016b57
Lpc-Bee <2100005012@nttu.edu.vn>
2024-08-21 12:48:58 +0700

    update

--- cognizant/src/component/Footer/FooterTop.js
+++ cognizant/src/component/Footer/FooterTop.js
@@ -8,6 +8,7 @@ const FooterTop = () => {
   return (
     <div className="footer-top-container">
       <div className="footer-top-content">
+      <div class="footer-row">
         <div className="footer-top-left">
           <h2>POWER 5 CLIENTS</h2>
           <p>EXPLORE SOME OF POWER 5’S CLIENTS</p>
@@ -17,6 +18,8 @@ const FooterTop = () => {
           <img src={logo2} className="footer-logo" alt="Vadanha Logo" />
           <img src={logo1} className="footer-logo" alt="The Hive Logo" />
         </div>
+        </div>
+       
       </div>
     </div>
   );
