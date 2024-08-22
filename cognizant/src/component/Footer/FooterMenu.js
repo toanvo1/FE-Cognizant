07554f638b03a49f3a7748e9c3fb2b76cb62bbe8
Lpc-Bee <2100005012@nttu.edu.vn>
2024-08-22 23:04:17 +0700

    fix footer

--- cognizant/src/component/Footer/FooterMenu.js
+++ cognizant/src/component/Footer/FooterMenu.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./Style/Menu.css";
-import {
-  PhoneOutlined,
-  MailOutlined,
-  EnvironmentOutlined,
-} from "@ant-design/icons";
+import { PhoneOutlined, MailOutlined, EnvironmentOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import logo from "../../image/logo.png";
 
@@ -12,14 +8,15 @@ const FooterMenu = () => {
   return (
     <div style={{ backgroundColor: "#044026", width: "100%" }}>
       <div className="container">
-        <footer className="footer-container">
+      <footer className="footer-container">
           <div className="footer-left">
             <img src={logo} className="footer-logo" alt="Logo" />
+            <h4 className="footer-company-name">Power 5 Company Limited</h4>
             <div className="footer-contact">
               <div className="contact-item">
                 <EnvironmentOutlined />
                 <span>
-                  <p>3rd Floor, Vietcom Building</p>
+                  3rd Floor, Vietcom Building
                   <p>7-9 Dang Thai Mai St, Ward 7, Phu Nhuan Dist, HCMC</p>
                 </span>
               </div>
@@ -32,64 +29,38 @@ const FooterMenu = () => {
                 <span>Info@power5.vn</span>
               </div>
             </div>
-            <div className="footer-right-bottom">
-              <span>Power5 © Copyright 2020, Inc. All rights reserved.</span>
-            </div>
           </div>
-          <div className="footer-right">
+          <div className="footer-middle">
             <div className="footer-column">
               <h4>Industries</h4>
-              <Link className="footer-link" to="#">
-                FAQ
-              </Link>
-              <Link className="footer-link" to="#">
-                Chính sách bảo mật
-              </Link>
-              <Link className="footer-link" to="#">
-                Hướng dẫn thanh toán
-              </Link>
-              <Link className="footer-link" to="#">
-                Kích hoạt khoá học
-              </Link>
-              <Link className="footer-link" to="#">
-                Điều khoản sử dụng
-              </Link>
+              <Link className="footer-link" to="#">Education</Link>
+              <Link className="footer-link" to="#">Retail</Link>
+              <Link className="footer-link" to="#">Oil & Gas</Link>
+              <Link className="footer-link" to="#">Transportation & Logistics</Link>
+              <Link className="footer-link" to="#">Communications, Media & Technology</Link>
             </div>
             <div className="footer-column">
               <h4>Services</h4>
-              <Link className="footer-link" to="#">
-                FAQ
-              </Link>
-              <Link className="footer-link" to="#">
-                Chính sách bảo mật
-              </Link>
-              <Link className="footer-link" to="#">
-                Hướng dẫn thanh toán
-              </Link>
-              <Link className="footer-link" to="#">
-                Kích hoạt khoá học
-              </Link>
-              <Link className="footer-link" to="#">
-                Điều khoản sử dụng
-              </Link>
+              <Link className="footer-link" to="#">Accounting</Link>
+              <Link className="footer-link" to="#">Consulting</Link>
+              <Link className="footer-link" to="#">Business Process</Link>
+              <Link className="footer-link" to="#">Coworking Space</Link>
+              <Link className="footer-link" to="#">Software Engineering</Link>
+              <Link className="footer-link" to="#">Cloud Solutions</Link>
+              <Link className="footer-link" to="#">Cybersecurity</Link>
+              <Link className="footer-link" to="#">Infrastructures</Link>
+              <Link className="footer-link" to="#">Web Design & Digital Marketing</Link>
             </div>
-            <div className="footer-column">
-              <h4>About us</h4>
-              <Link className="footer-link" to="#">
-                FAQ
-              </Link>
-              <Link className="footer-link" to="#">
-                Chính sách bảo mật
-              </Link>
-              <Link className="footer-link" to="#">
-                Hướng dẫn thanh toán
-              </Link>
-              <Link className="footer-link" to="#">
-                Kích hoạt khoá học
-              </Link>
-              <Link className="footer-link" to="#">
-                Điều khoản sử dụng
-              </Link>
+          </div>
+          <div className="footer-right">
+            <div className="footer-column about">
+              <h4>About</h4>
+              <Link className="footer-link" to="#">About Power 5</Link>
+              <Link className="footer-link" to="#">Clients & Partnerships</Link>
+            </div>
+            <div className="footer-column contact">
+              <h4>Contact</h4>
+              <Link className="footer-link" to="#">Contact Us</Link>
             </div>
           </div>
         </footer>
--- cognizant/src/component/Footer/FooterTop.js
+++ cognizant/src/component/Footer/FooterTop.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Style/FooterTop.css";
-import logo1 from "../../image/logo-thehive.png";
-import logo2 from "../../image/logo-vadanha.png";
-import logo3 from "../../image/logo-waren.png";
+import logo1 from "../../image/seb.png";
+import logo2 from "../../image/edutek.png";
+import logo3 from "../../image/tesse.png";
 
 const FooterTop = () => {
   return (
@@ -12,13 +12,13 @@ const FooterTop = () => {
           <div className="footer-top-content">
             <div class="footer-row">
               <div className="footer-top-left">
-                <h2>POWER 5 CLIENTS</h2>
-                <p>EXPLORE SOME OF POWER 5’S CLIENTS</p>
+                <h2>TRUSTED BY THE BEST</h2>
+                <p>Discover the clients we have worked with</p>
               </div>
               <div className="footer-top-right">
-                <img src={logo3} className="footer-logo" alt="Waren Logo" />
+                <img src={logo1} className="footer-logo" alt="Waren Logo" />
                 <img src={logo2} className="footer-logo" alt="Vadanha Logo" />
-                <img src={logo1} className="footer-logo" alt="The Hive Logo" />
+                <img src={logo3} className="footer-logo" alt="The Hive Logo" />
               </div>
             </div>
           </div>
